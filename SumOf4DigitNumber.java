190d68a6eb141c164646b7f2a9b10eb38f8fce8b
sheetalbmohite <sheetalbmohite@gmail.com>
2025-03-27 17:40:13 +0530

    Array problems batch 1

--- SumOf4DigitNumber.java
+++ SumOf4DigitNumber.java
@@ -13,8 +13,8 @@ public class SumOf4DigitNumber {
 
     static void sumOfDigit(int num){
 
-        Array.toString(num);
-        int sorted = sort(num);
+//        Array.toString(num);
+//        int sorted = sort(num);
     }
     public static void main(String []args){
         int num = 5361; //least will be 13+51 => 64 but when small num is there in 10s place of both numbers 1_ | 3_ => 15 +36 => 51.
